7fe4079b9b170f28d5ce12f8f32ab0ae77473d9e
Pablo Agustin Torres Lell <lmgb.torreslell.pablo@gmail.com>
2025-10-02 09:37:06 -0300

    Se modifican .css de Header & Footer

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,29 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     // Clase 'header'
     <header className="header">
-      <div className="logo">
-        <img
-          src="/img/logo.svg" 
-          alt="Hermanos Jota Logo"
-          className="logo-image"
-        />
-      </div>
-      
-      <nav className="nav">
-        {/* Rutas de navegación */}
-        <Link to="/">Inicio</Link> 
-        <Link to="/catalogo">Productos</Link>
-        <Link to="/nosotros">Nosotros</Link>
-        <Link to="/contacto">Contacto</Link>
-      </nav>
+      <div className="container">
+        <div className="logo">
+          <img
+            src="/img/logo.svg"
+            alt="Hermanos Jota Logo"
+            className="logo-image"
+          />
+        </div>
 
-      <div className="header-icons">
-        <Link to="/login" className="icon-link"><i className="fa-solid fa-user"></i></Link>
-        <Link to="/carrito" className="icon-link"><i className="fa-solid fa-shopping-cart"></i></Link>
+        <nav className="nav">
+          {/* Rutas de navegación */}
+          <Link to="/">Inicio</Link>
+          <Link to="/catalogo">Productos</Link>
+          <Link to="/nosotros">Nosotros</Link>
+          <Link to="/contacto">Contacto</Link>
+        </nav>
+
+        <div className="header-icons">
+          <Link to="/login" className="icon-link">
+            <i className="fa-solid fa-user"></i>
+          </Link>
+          <Link to="/carrito" className="icon-link">
+            <i className="fa-solid fa-shopping-cart"></i>
+          </Link>
+        </div>
       </div>
     </header>
   );
