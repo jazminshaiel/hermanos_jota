ae65ad587c7476c796b8e82f8cd17f063c9577bb
Pablo Agustin Torres Lell <lmgb.torreslell.pablo@gmail.com>
2025-10-02 14:34:46 -0300

    Se agrega base de ProductDetail & Cart

---
+++ client/src/pages/ProductDetail.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function ProductDetail() {
+  return (
+    <>
+      <Header />
+      <div></div>
+      <Footer />
+    </>
+  );
+}
+export default ProductDetail;
