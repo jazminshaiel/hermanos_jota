806bdbcec224718d0dd2a27b85b6b3064c8c3688
Pablo Agustin Torres Lell <lmgb.torreslell.pablo@gmail.com>
2025-10-02 13:52:05 -0300

    Se mejora footer (Footer.jsx)

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,9 @@ const Footer = () => {
   return (
     // Clase 'footer'
     <footer className="footer">
-      <div className="container"> 
+      <div className="container">
+
+        {/* Logo e info de empresa */} 
         <div className="footer-content-wrapper">
           
           <div className="footer-section">
@@ -18,22 +20,36 @@ const Footer = () => {
             </div>
             <div className="company-info">
               <p>Hermanos Jota - Casa Taller</p>
-              <p>Av. San Juan 2847</p>
-              {/* ... más info ... */}
+            <p>Av. San Juan 2847</p>
+            <p>C1232AAB - Barrio de San Cristóbal</p>
+            <p>Ciudad Autónoma de Buenos Aires</p>
+            <p>Argentina</p>
+            <p>+54 11 4567-8901</p>
+            <p>contacto@hermanosjota.com</p>
             </div>
           </div>
 
+          {/* Redes sociales */}
           <div className="footer-section">
             <h4>Redes Sociales</h4>
-            <a href="URL_INSTAGRAM" target="_blank" rel="noopener noreferrer">Instagram</a>
+            <a href="https://instagram.com/hermanosjota" target="_blank" rel="noreferrer">Instagram: @hermanosjota_ba</a>
           </div>
 
+          {/* Ayuda + Newsletter */}
           <div className="footer-section">
             <h4>Ayuda</h4>
             <Link to="/pagos">Métodos de pago</Link>
             <Link to="/cambios">Cambios y devoluciones</Link>
             <Link to="/privacidad">Politicas de privacidad</Link>
-            {/* ... Newsletter ... */}
+            
+            <div className="newsletter">
+            <h4>Newsletter</h4>
+            <div className="newsletter-input">
+              <input type="email" placeholder="Ingresá tu email" />
+              <button>Suscribirse</button>
+            </div>
+          </div>
+
           </div>
 
         </div> 
